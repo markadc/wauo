87bb222c54257b0b82b1c10c1d1c96165a78bade
markadc <894028891@qq.com>
2024-05-05 18:22:48 +0800

    ...

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ url = 'https://www.baidu.com'
 resp: Response = s.send(url)
 
 print(resp)
-print(resp.request.headers['Cookie'])
+print(resp.request.headers['Cookie'])  # 输出：Your Cookies
 
 title = resp.get_one('//title/text()')
 print(title)  # 输出：百度一下，你就知道
