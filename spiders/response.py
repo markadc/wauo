fbaa0c20a45eb3fb9b6c3cf03d3cc9596401f71a
markadc <markadc@126.com>
2024-08-14 22:52:42 +0800

    一键提交

--- spiders/response.py
+++ spiders/response.py
@@ -8,23 +8,23 @@ class StrongResponse(Response):
     def __init__(self, response: Response):
         super().__init__()
         self.__dict__.update(response.__dict__)
-        self.seler = Selector(text=response.text)
+        self.selector = Selector(text=response.text)
 
     def __str__(self):
         return '<Response {}>'.format(self.status_code)
 
     def xpath(self, query: str):
-        sel = self.seler.xpath(query)
+        sel = self.selector.xpath(query)
         return sel
 
     def css(self, query: str):
-        sel = self.seler.css(query)
+        sel = self.selector.css(query)
         return sel
 
     def get_one(self, query: str, default=''):
-        value = self.seler.xpath(query).get(default).strip()
+        value = self.selector.xpath(query).get(default).strip()
         return value
 
     def get_all(self, query: str):
-        value = [v.strip() for v in self.seler.xpath(query).getall()]
+        value = [v.strip() for v in self.selector.xpath(query).getall()]
         return value
