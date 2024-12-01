9a457a81600bf3e03db4eabedc4b457807b8d2ac
markadc <markadc@126.com>
2024-12-01 16:32:50 +0800

    改为__logger

--- utils/loger.py
+++ utils/loger.py
@@ -5,9 +5,9 @@ from loguru import logger
 
 class Loger:
     def __init__(self, level="INFO", file=None):
-        self.logger = logger
+        self.__logger = logger
         self.remove()
         self.add(file or sys.stdout, level=level)  # 设置日志级别
 
     def __getattr__(self, item):
-        return getattr(self.logger, item)
+        return getattr(self.__logger, item)
