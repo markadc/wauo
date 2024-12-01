e8c7217429fab9a4693fe283210e6f2db0f71395
markadc <markadc@126.com>
2024-12-01 16:19:09 +0800

    新增Loger

---
+++ utils/loger.py
@@ -0,0 +1,13 @@
+import sys
+
+from loguru import logger
+
+
+class Loger:
+    def __init__(self, level="INFO", file=None):
+        self.logger = logger
+        self.remove()
+        self.add(file or sys.stdout, level=level)  # 设置日志级别
+
+    def __getattr__(self, item):
+        return getattr(self.logger, item)
