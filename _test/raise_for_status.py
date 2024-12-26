63c8610bba6759a6a2bd06754d65fb3610c5e707
markadc <markadc@126.com>
2024-12-26 17:48:27 +0800

    .

--- _test/raise_for_status.py
+++ _test/raise_for_status.py
@@ -3,7 +3,4 @@ from wauo import WauoSpider
 s = WauoSpider()
 url = 'http://www.baidu.com'
 r = s.do(url)
-try:
-    r.raise_for_status(codes=[301, 302])
-except Exception as e:
-    print("{!r} {!r}".format(type(e).__name__, e))
+r.raise_for_status(codes=[301, 302])
