3ea9731df8169c09cbd0cd4f5f11ea7b349e0d46
markadc <markadc@126.com>
2024-12-26 15:38:14 +0800

    新增raise_for_status、raise_for_text

--- spiders/response.py
+++ spiders/response.py
@@ -1,5 +1,9 @@
+from typing import Callable
+
 from parsel import Selector
 from requests import Response
+
+from wauo.spiders.errors import ResponseCodeError, ResponseTextError
 
 
 class StrongResponse(Response):
@@ -28,3 +32,12 @@ class StrongResponse(Response):
     def get_all(self, query: str):
         value = [v.strip() for v in self.selector.xpath(query).getall()]
         return value
+
+    def raise_for_status(self, codes: list = None):
+        codes = codes or [200]
+        if self.status_code not in codes:
+            raise ResponseCodeError("{} not in {}".format(self.status_code, codes))
+
+    def raise_for_text(self, validate: Callable[[str], bool] = None):
+        if validate(self.text) is False:
+            raise ResponseTextError("not ideal text")
