9d333a35eab408b975c55e9c4ae4cebca7f74095
markadc <markadc@126.com>
2025-01-07 15:48:35 +0800

    U Loger

--- utils/loger.py
+++ utils/loger.py
@@ -11,3 +11,18 @@ class Loger:
 
     def __getattr__(self, item):
         return getattr(self.__logger, item)
+
+    def debug(self, msg, *args, **kwargs):
+        self.__logger.debug(msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        self.__logger.info(msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        self.__logger.warning(msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        self.__logger.error(msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        self.__logger.critical(msg, *args, **kwargs)
