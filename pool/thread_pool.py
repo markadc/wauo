cbde6a68349ea08e6f4f2eb609bf99dc19873643
markadc <markadc@126.com>
2025-11-19 12:51:01 +0800

    优化线程池

--- pool/thread_pool.py
+++ pool/thread_pool.py
@@ -1,8 +1,13 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
-from concurrent.futures import ThreadPoolExecutor
 
 
-class LimitedThreadPool:
+class SmartThreadPool:
+    """
+    智能线程池
+    - 当有任务提交时，如果达到了最大并发数，则阻塞，直到有线程执行结束释放了资源
+    """
+
     def __init__(self, max_workers=10):
         self.max_workers = max_workers
         self.pool = ThreadPoolExecutor(max_workers=max_workers)
@@ -12,8 +17,7 @@ class LimitedThreadPool:
     def submit(self, fn, *args, **kwargs):
         """提交任务，如果达到最大并发数则阻塞"""
         with self.condition:
-            # 等待直到有空闲线程
-            self.condition.wait_for(lambda: self.current_tasks < self.max_workers)
+            self.condition.wait_for(lambda: self.current_tasks < self.max_workers)  # 等待，直到有空闲线程
             self.current_tasks += 1
             future = self.pool.submit(fn, *args, **kwargs)
             future.add_done_callback(self._task_done)
@@ -25,25 +29,59 @@ class LimitedThreadPool:
             self.current_tasks -= 1
             self.condition.notify()  # 通知等待的线程
 
+    def map(self, fn, *iterables, timeout=None):
+        """
+        Args:
+            fn: 要执行的函数
+            *iterables: 一个或多个可迭代对象
+            timeout: 超时时间（秒）
+        Returns:
+            返回结果的迭代器（先完成的先返回）
+        """
+        futures = [self.submit(fn, *args) for args in zip(*iterables)]
+        for future in as_completed(futures, timeout=timeout):
+            yield future.result()
+
     def shutdown(self):
         """关闭线程池"""
         self.pool.shutdown()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.shutdown()
+        return False
 
 
 if __name__ == "__main__":
     import time, random
     from wauo import printer as p
 
+    def job(i):
+        p.yellow(f"{i} 执行中...")
+        delay = random.uniform(1, 5)
+        time.sleep(delay)
+        p.green(f"✅ {i} 已完成")
+        return delay, i
 
-    def work(x):
-        p.yellow(f'{x} 执行中')
-        time.sleep(random.uniform(0, 3))
-        p.green(f'{x} 已完成')
+    # 方式1
+    with SmartThreadPool(max_workers=5) as pool:
+        for i in range(10):
+            pool.submit(job, i)
 
+    # 方式2
+    with SmartThreadPool(max_workers=10) as pool:
+        pool.map(job, range(10))
 
-    pool = LimitedThreadPool(max_workers=3)
+    # 获取线程返回值
+    # with SmartThreadPool(max_workers=5) as pool:
+    #     fs = [pool.submit(job, i) for i in range(10)]
+    #     for f in fs:
+    #         print(f.result())
 
-    for i in range(10):
-        pool.submit(work, i + 1)
-
-    pool.shutdown()
+    # 获取线程返回值（先完成的线程先返回）
+    # with SmartThreadPool(max_workers=10) as pool:
+    #     results = pool.map(job, range(10))
+    #     for result in results:
+    #         print(result)
